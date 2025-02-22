32b122f64595841a4e35fb611d068d8a9364b989
matheus.rodrigues <matheus.rodrigues@tecnospeed.com.br>
2025-02-21 21:07:40 -0300

    chore: Validação de variáveis de ambiente

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,12 +1,34 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import z from 'zod';
 
-const env = `.env.${process.env.NODE_ENV}`;
+const envFile = `.env.${process.env.NODE_ENV}`;
 const pathEnv =
   process.env.NODE_ENV === 'local'
-    ? path.resolve(env)
-    : path.resolve(__dirname, '../../', env);
+    ? path.resolve(envFile)
+    : path.resolve(__dirname, '../../', envFile);
 
 dotenv.config({
   path: pathEnv,
 });
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['local', 'test', 'homolog', 'staging', 'production'])
+    .default('local'),
+  PORT: z.coerce.number(),
+  REDIS_URL: z.string().min(10).default(''),
+  REDIS_PASSWORD: z.string().optional(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  const error = new Error('❌ Invalid environment variables.', {
+    cause: _env.error.format(),
+  });
+
+  throw error;
+}
+
+export const env = _env.data;
